1861748f6e7ecd000d5d2bd1792e1b5292a5738f
gauravpixxelu <gauravpixxelu@gmail.com>
2023-06-23 16:32:03 +0530

    change

--- src/Components/UnlockYourPotential.jsx
+++ src/Components/UnlockYourPotential.jsx
@@ -40,6 +40,21 @@ function UnlockYourPotential() {
 
             {/* Potential Data Ends */}
 
+             {/* Potential Data Starts */}
+
+             <section className="potential-data">
+                <div className="container">
+                <h2 className="hdng">Well, we have the solution!</h2>
+                <p className="hdng-16">RED3SIXTY helps accountants position themselves as trusted consultants by providing them with industry-specific insights, methodologies, and expertise. The platform equips accountants with the knowledge and tools to demonstrate their holistic financial guidance and consulting capabilities, building trust with clients seeking comprehensive solutions.</p>
+                <p className="hdng-16">RED3SIXTY provides a comprehensive framework and guidance to help accountants transition from charging for hours to charging for value. The platform offers tools to assess the value provided to clients, establish pricing structures, and communicate the value proposition effectively.</p>
+                <p className="hdng-16">RED3SIXTY's platform utilizes AI and data analytics to help accountants identify targeted consulting opportunities within specific industries or niches. It provides insights into industry trends, challenges, and client needs, enabling accountants to focus their efforts and deliver tailored consulting solutions.</p>
+                <p className="hdng-16">RED3SIXTY's platform provides tools for efficient project management and resource allocation. Accountants can leverage these features to effectively balance their time and resources between providing traditional accounting services and delivering consulting projects, optimizing productivity and client satisfaction.</p>
+                <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
+                </div>
+            </section>
+
+            {/* Potential Data Ends */}
+
 
 
         </div>
