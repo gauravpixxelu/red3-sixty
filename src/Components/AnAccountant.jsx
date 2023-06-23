496e5eabd80907907fe05e9bee3be21ce29568bf
gauravpixxelu <gauravpixxelu@gmail.com>
2023-06-23 17:45:54 +0530

    newcommit

--- src/Components/AnAccountant.jsx
+++ src/Components/AnAccountant.jsx
@@ -14,23 +14,23 @@ function AnAccountant() {
 
             <section className="accontant-block">
                 <div className="container">
-                    <div className="acc-hndg">
+                    <div className="acc-hndg" data-aos="fade-up">
                         <h2 className="sec-hdng">Unlock Success with RED3SIXTY: Empowering Accountants for Remarkable Results!</h2>
                         <p className="hdng-16">Are you ready to take your accounting practice to the next level? Say goodbye to average results and embrace the power of RED3SIXTY – the game-changing platform that delivers extraordinary outcomes for accountants like you!</p>
                     </div>
 
                     <div className="acc-graph">
-                        <div className="acc-box">
+                        <div className="acc-box" data-aos="fade-left">
                             <h4>Achieve Unparalleled Results</h4>
                             <img src={RightArrow} alt="RightArrow" />
                         </div>
 
-                        <div className="acc-box">
+                        <div className="acc-box" data-aos="fade-left">
                             <h4>Multiply Your Lead Generation</h4>
                             <img src={RightArrow} alt="RightArrow" />
                         </div>
                         <img src={RightArrow} alt="RightArrow" />
-                        <div className="acc-box">
+                        <div className="acc-box" data-aos="fade-left">
                             <h4>Unleash a Phenomenal ROI</h4>
                         </div>
                     </div>
@@ -44,39 +44,39 @@ function AnAccountant() {
 
             <section className="accontant-data">
                 <div className="container">
-                    <div className="accont-blocks">
+                    <div className="accont-blocks" data-aos="fade-up">
                         <h2 className="hdng">Achieve Unparalleled Results:</h2>
                         <p className="hdng-16">With RED3SIXTY, you can achieve remarkable results that will transform your business. Experience a 33% increase in your close rate and watch your profits soar by 27%. Imagine the financial growth and prosperity that awaits you!</p>
                     </div>
 
-                    <div className="accont-blocks">
+                    <div className="accont-blocks" data-aos="fade-up">
                         <h2 className="hdng">Multiply Your Lead Generation:</h2>
                         <p className="hdng-16">Expand your client base like never before with RED3SIXTY. Our platform has the power to increase your lead generation by a staggering 300%. Connect with more potential clients, showcase your expertise, and skyrocket your success!</p>
                         <p className="hdng-16">RED3SIXTY offers a comprehensive suite of services that will revolutionize your consulting practice. From unlimited diagnostics and report generation to lead generation and enhanced efficiency, our platform has everything you need to take your business to the next level</p>
-                        <NavLink to="#" className="custom-btn"><span>Unlock Your Potential Now!</span></NavLink>
+                        <NavLink to="/potential" className="custom-btn"><span>Unlock Your Potential Now!</span></NavLink>
                     </div>
 
-                    <div className="accont-blocks">
+                    <div className="accont-blocks" data-aos="fade-up">
                         <h2 className="hdng">Unleash a Phenomenal ROI:</h2>
                         <p className="hdng-16">The initial cost of signing up for RED3SIXTY is only $199, but the return on investment (ROI) is beyond imagination. You'll generate an ROI of approximately 6,655% – that's over 66 times the initial investment! Prepare for unprecedented financial gains and a thriving practice.</p>
                     </div>
 
-                    <div className="accont-blocks">
+                    <div className="accont-blocks" data-aos="fade-up">
                         <h2 className="hdng">A Platform Tailored for Accountants:</h2>
                         <p className="hdng-16">RED3SIXTY understands the unique needs of accountants. We offer unlimited report generation, diagnostics, and engagements, ensuring you have the tools to deliver exceptional value to your clients. With our platform, you'll elevate your services, differentiate yourself from the competition, and position yourself as a trusted advisor.</p>
                     </div>
 
-                    <div className="accont-blocks">
+                    <div className="accont-blocks" data-aos="fade-up">
                         <h2 className="hdng">Embrace the Future of Accounting:</h2>
                         <p className="hdng-16">The accounting landscape is evolving, and RED3SIXTY is here to equip you with the tools and capabilities to thrive in this dynamic environment. Experience streamlined processes, data-driven insights, and enhanced efficiency – all while delivering exceptional results that set you apart.</p>
                     </div>
 
-                    <div className="accont-blocks">
+                    <div className="accont-blocks" data-aos="fade-up">
                         <h2 className="hdng">Join the RED3SIXTY Revolution Today:</h2>
                         <p className="hdng-16">Don't miss out on this opportunity to unlock your full potential and achieve remarkable success. Take the leap and join the RED3SIXTY revolution. Click the button below to discover how our platform can transform your accounting practice!</p>
                         <p className="hdng-16">RED3SIXTY offers a comprehensive suite of services that will revolutionize your consulting practice. From unlimited diagnostics and report generation to lead generation and enhanced efficiency, our platform has everything you need to take your business to the next level</p>
                         <p className="hdng-16">Prepare for a future of extraordinary growth, financial success, and unlimited possibilities. With RED3SIXTY by your side, you'll be unstoppable!</p>
-                        <NavLink to="#" className="custom-btn"><span>Unlock Your Potential Now!</span></NavLink>
+                        <NavLink to="/potential" className="custom-btn"><span>Unlock Your Potential Now!</span></NavLink>
                     </div>
 
 
--- src/Components/UnlockYourPotential.jsx
+++ src/Components/UnlockYourPotential.jsx
@@ -12,64 +12,86 @@ function UnlockYourPotential() {
 
             {/* Potential Data Starts */}
 
+            <section className="potential-data potential-bg" >
+                <div className="container" data-aos="fade-up">
+                    <h2 className="hdng">Do the following problems resonate with you?</h2>
+                    <p className="hdng-16">"I struggle with marketing and promoting my consulting services within my existing accounting client base, limiting awareness of the value-added services I can provide beyond traditional accounting."</p>
+                    <p className="hdng-16">"I struggle with incorporating business diagnostics into my accounting and consulting approach, which hampers my ability to provide comprehensive insights and tailored solutions to clients."</p>
+                    <p className="hdng-16">"I lack a structured approach or methodology for incorporating consulting into my existing accounting practice, resulting in inconsistencies or inefficiencies in delivering consulting services to clients."</p>
+                    <p className="hdng-16">"I lack efficient and standardized processes for integrating business diagnostics into my accounting and consulting services, leading to inefficiencies and inconsistencies in delivering insights to clients."</p>
+                    <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
+                </div>
+            </section>
+
+            {/* Potential Data Ends */}
+
+            {/* Potential Data Starts */}
+
+            <section className="potential-data">
+                <div className="container" data-aos="fade-up">
+                    <h2 className="hdng">Well, we have the solution!</h2>
+                    <p className="hdng-16">RED3SIXTY helps accountants position themselves as trusted consultants by providing them with industry-specific insights, methodologies, and expertise. The platform equips accountants with the knowledge and tools to demonstrate their holistic financial guidance and consulting capabilities, building trust with clients seeking comprehensive solutions.</p>
+                    <p className="hdng-16">RED3SIXTY provides a comprehensive framework and guidance to help accountants transition from charging for hours to charging for value. The platform offers tools to assess the value provided to clients, establish pricing structures, and communicate the value proposition effectively.</p>
+                    <p className="hdng-16">RED3SIXTY's platform utilizes AI and data analytics to help accountants identify targeted consulting opportunities within specific industries or niches. It provides insights into industry trends, challenges, and client needs, enabling accountants to focus their efforts and deliver tailored consulting solutions.</p>
+                    <p className="hdng-16">RED3SIXTY's platform provides tools for efficient project management and resource allocation. Accountants can leverage these features to effectively balance their time and resources between providing traditional accounting services and delivering consulting projects, optimizing productivity and client satisfaction.</p>
+                    <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
+                </div>
+            </section>
+
+            {/* Potential Data Ends */}
+
+            {/* Potential Data Starts */}
+
             <section className="potential-data potential-bg">
-                <div className="container">
-                <h2 className="hdng">Do the following problems resonate with you?</h2>
-                <p className="hdng-16">"I struggle with marketing and promoting my consulting services within my existing accounting client base, limiting awareness of the value-added services I can provide beyond traditional accounting."</p>
-                <p className="hdng-16">"I struggle with incorporating business diagnostics into my accounting and consulting approach, which hampers my ability to provide comprehensive insights and tailored solutions to clients."</p>
-                <p className="hdng-16">"I lack a structured approach or methodology for incorporating consulting into my existing accounting practice, resulting in inconsistencies or inefficiencies in delivering consulting services to clients."</p>
-                <p className="hdng-16">"I lack efficient and standardized processes for integrating business diagnostics into my accounting and consulting services, leading to inefficiencies and inconsistencies in delivering insights to clients."</p>
-                <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
+                <div className="container" data-aos="fade-up">
+                    <h2 className="hdng">What’s the Return on Investment?</h2>
+                    <p className="hdng-16">Target - Improved close rate by 33% and increased profits by 27%<br />
+                        Target - Increase Lead Generation by 300%<br />
+                        Initial Cost = $199<br />
+                        Net Increase in Revenue = $13,460 (20 new once off customers ($100)and 5 consulting engagements ($191))<br />
+                        ROI = ($13,460 - $199) / $199 * 100 <br />
+                        ROI = $13,261 / $199 * 100<br />
+                        ROI ≈ 6,655% (Month 1)
+                    </p>
+                    <p className="hdng-16">In this scenario, the ROI is approximately 6,655%. This means that the accountant would generate a return on investment that is more than 66 times the initial cost of signing up for RED3SIXTY.</p>
+                    <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
                 </div>
             </section>
 
             {/* Potential Data Ends */}
 
-                        {/* Potential Data Starts */}
+            {/* Potential Data Starts */}
 
-                        <section className="potential-data">
-                <div className="container">
-                <h2 className="hdng">Well, we have the solution!</h2>
-                <p className="hdng-16">RED3SIXTY helps accountants position themselves as trusted consultants by providing them with industry-specific insights, methodologies, and expertise. The platform equips accountants with the knowledge and tools to demonstrate their holistic financial guidance and consulting capabilities, building trust with clients seeking comprehensive solutions.</p>
-                <p className="hdng-16">RED3SIXTY provides a comprehensive framework and guidance to help accountants transition from charging for hours to charging for value. The platform offers tools to assess the value provided to clients, establish pricing structures, and communicate the value proposition effectively.</p>
-                <p className="hdng-16">RED3SIXTY's platform utilizes AI and data analytics to help accountants identify targeted consulting opportunities within specific industries or niches. It provides insights into industry trends, challenges, and client needs, enabling accountants to focus their efforts and deliver tailored consulting solutions.</p>
-                <p className="hdng-16">RED3SIXTY's platform provides tools for efficient project management and resource allocation. Accountants can leverage these features to effectively balance their time and resources between providing traditional accounting services and delivering consulting projects, optimizing productivity and client satisfaction.</p>
-                <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
+            <section className="potential-data">
+                <div className="container" data-aos="fade-up">
+                    <h2 className="hdng">What do you get?</h2>
+                    <p className="hdng-16">We have developed business growth platforms aimed at helping you run diagnostics and determine where your customer needs to focus in order to grow their business. You get:</p>
+                    <ul className="site-ul">
+                     <li><b>1.</b>A lead generating system guaranteed to provide you unlimited qualified leads</li>   
+                     <li><b>2.</b>Business diagnostics proven to convert leads into paying customers</li>  
+                     <li><b>3.</b>Unlimited diagnostics and auto completed reports ready to sell to your clients</li>  
+                     <li><b>4.</b>Automated development platform with more the 40 tools and 100’s hours of training content and videos</li>  
+                     <li><b>5.</b>Training and delivery manuals making delivery of any business solution easy</li>  
+                     <li><b>6.</b>Online certification ensuring you are certified and ready to start delivering </li>  
+                    </ul>
+                    <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
                 </div>
             </section>
 
             {/* Potential Data Ends */}
 
-             {/* Potential Data Starts */}
 
-             <section className="potential-data">
-                <div className="container">
-                <h2 className="hdng">Well, we have the solution!</h2>
-                <p className="hdng-16">RED3SIXTY helps accountants position themselves as trusted consultants by providing them with industry-specific insights, methodologies, and expertise. The platform equips accountants with the knowledge and tools to demonstrate their holistic financial guidance and consulting capabilities, building trust with clients seeking comprehensive solutions.</p>
-                <p className="hdng-16">RED3SIXTY provides a comprehensive framework and guidance to help accountants transition from charging for hours to charging for value. The platform offers tools to assess the value provided to clients, establish pricing structures, and communicate the value proposition effectively.</p>
-                <p className="hdng-16">RED3SIXTY's platform utilizes AI and data analytics to help accountants identify targeted consulting opportunities within specific industries or niches. It provides insights into industry trends, challenges, and client needs, enabling accountants to focus their efforts and deliver tailored consulting solutions.</p>
-                <p className="hdng-16">RED3SIXTY's platform provides tools for efficient project management and resource allocation. Accountants can leverage these features to effectively balance their time and resources between providing traditional accounting services and delivering consulting projects, optimizing productivity and client satisfaction.</p>
-                <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
+            {/* Potential Data Starts */}
+
+            <section className="potential-data potential-bg">
+                <div className="container" data-aos="fade-up">
+                    <h2 className="hdng">Our Vision</h2>
+                    <p className="hdng-16">At RED3SIXTY, our vision is to revolutionize the consulting industry by empowering consultants, accountants, and coaches with cutting-edge technology and comprehensive solutions. We strive to be the premier platform that drives business growth, transforms client outcomes, and inspires excellence in the consulting profession. Through our innovative tools, unlimited capabilities, and unwavering commitment to value, we envision a future where every consultant has the power to unlock their full potential, deliver exceptional results, and make a lasting impact on businesses worldwide..</p>
+                    <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
                 </div>
             </section>
 
             {/* Potential Data Ends */}
-
-                      {/* Potential Data Starts */}
-
-                      <section className="potential-data">
-                <div className="container">
-                <h2 className="hdng">Well, we have the solution!</h2>
-                <p className="hdng-16">RED3SIXTY helps accountants position themselves as trusted consultants by providing them with industry-specific insights, methodologies, and expertise. The platform equips accountants with the knowledge and tools to demonstrate their holistic financial guidance and consulting capabilities, building trust with clients seeking comprehensive solutions.</p>
-                <p className="hdng-16">RED3SIXTY provides a comprehensive framework and guidance to help accountants transition from charging for hours to charging for value. The platform offers tools to assess the value provided to clients, establish pricing structures, and communicate the value proposition effectively.</p>
-                <p className="hdng-16">RED3SIXTY's platform utilizes AI and data analytics to help accountants identify targeted consulting opportunities within specific industries or niches. It provides insights into industry trends, challenges, and client needs, enabling accountants to focus their efforts and deliver tailored consulting solutions.</p>
-                <p className="hdng-16">RED3SIXTY's platform provides tools for efficient project management and resource allocation. Accountants can leverage these features to effectively balance their time and resources between providing traditional accounting services and delivering consulting projects, optimizing productivity and client satisfaction.</p>
-                <NavLink to="/contact" className="custom-btn"><span>Book an online demo Now!</span></NavLink>
-                </div>
-            </section>
-
-            {/* Potential Data Ends */}
-
 
 
         </div>
