33b250fe0130bf24c57c51f198f416b7559ee978
gauravpixxelu <gauravpixxelu@gmail.com>
2023-06-29 15:45:48 +0530

    klkl

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Notfound from '../assets/images/404.png';
+import Notfound from '../assets/images/404.jpg';
 
 const NotFound = () => {
   return (
-    <section className="404-block" >
-      <img src={Notfound} alt="Notfound"/>
+    <section className="notfoundblock" >
+      <div className="container">
+        <img src={Notfound} alt="Notfound" />
+      </div>
     </section>
   );
 };
