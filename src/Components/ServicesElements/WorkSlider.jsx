426b3a7c7ea58467b3338c61f20d480c519865e4
gauravpixxelu <gauravpixxelu@gmail.com>
2023-08-05 15:21:55 +0530

    new

--- src/Components/ServicesElements/WorkSlider.jsx
+++ src/Components/ServicesElements/WorkSlider.jsx
@@ -43,54 +43,48 @@ const WorkSlider = () => {
             <div className="work-slide">
                 <img src={Work1} alt="Review1" />
                 <div className="work-slide-content">
-                    <h4 class="small-hdn">EKOCOCO</h4>
-                    <h4 class="hdng-h4">Coconut tree waste material crafts</h4>
-                    <p className="hdng-16">Designed ELE4ORCE website, increasing client's customer base by 35% with visually appealing and user-friendly interface.</p>
+                    <h4 class="small-hdn">Wordpress</h4>
+                    <h4 class="hdng-h4">Ekococo</h4>
                 </div>
             </div>
 
             <div className="work-slide">
                 <img src={Work2} alt="Review2" />
                 <div className="work-slide-content">
-                    <h4 class="small-hdn">ELE4ORCE</h4>
-                    <h4 class="hdng-h4">New customers increase by 35%</h4>
-                    <p className="hdng-16">Collaborated with client to create unique website showcasing coconut tree waste material products. Result was professional and successful online platform.</p>
+                    <h4 class="small-hdn">Wordpress</h4>
+                    <h4 class="hdng-h4">Ele4orce</h4>
                 </div>
             </div>
 
             <div className="work-slide">
                 <img src={Work3} alt="Review3" />
                 <div className="work-slide-content">
-                    <h4 class="small-hdn">Krazy Deals & Steals</h4>
-                    <h4 class="hdng-h4">Sales increased by 30%</h4>
-                    <p className="hdng-16">Redesigned app for client, resulting in a significant boost in sales performance.</p>
+                    <h4 class="small-hdn">UI/UX Design</h4>
+                    <h4 class="hdng-h4">Krazy Deals & Steals</h4>
                 </div>
             </div>
 
             <div className="work-slide">
                 <img src={Work1} alt="Review1" />
                 <div className="work-slide-content">
-                    <h4 class="small-hdn">EKOCOCO</h4>
-                    <h4 class="hdng-h4">Coconut tree waste material crafts</h4>
-                    <p className="hdng-16">Designed ELE4ORCE website, increasing client's customer base by 35% with visually appealing and user-friendly interface.</p>
+                    <h4 class="small-hdn">Wordpress</h4>
+                    <h4 class="hdng-h4">Ekococo</h4>
                 </div>
             </div>
 
             <div className="work-slide">
                 <img src={Work2} alt="Review2" />
                 <div className="work-slide-content">
-                    <h4 class="small-hdn">ELE4ORCE</h4>
-                    <h4 class="hdng-h4">New customers increase by 35%</h4>
-                    <p className="hdng-16">Collaborated with client to create unique website showcasing coconut tree waste material products. Result was professional and successful online platform.</p>
+                    <h4 class="small-hdn">Wordpress</h4>
+                    <h4 class="hdng-h4">Ele4orce</h4>
                 </div>
             </div>
 
             <div className="work-slide">
                 <img src={Work3} alt="Review3" />
                 <div className="work-slide-content">
-                    <h4 class="small-hdn">Krazy Deals & Steals</h4>
-                    <h4 class="hdng-h4">Sales increased by 30%</h4>
-                    <p className="hdng-16">Redesigned app for client, resulting in a significant boost in sales performance.</p>
+                    <h4 class="small-hdn">UI/UX Design</h4>
+                    <h4 class="hdng-h4">Krazy Deals & Steals</h4>
                 </div>
             </div>
 
