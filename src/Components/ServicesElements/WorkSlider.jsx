ccafe174a6745267d2b0eb82e66671f4d646690e
gauravpixxelu <gauravpixxelu@gmail.com>
2023-08-09 15:28:09 +0530

    op

--- src/Components/ServicesElements/WorkSlider.jsx
+++ src/Components/ServicesElements/WorkSlider.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Work1 from '../ServicesElements/images/work1.jpg';
