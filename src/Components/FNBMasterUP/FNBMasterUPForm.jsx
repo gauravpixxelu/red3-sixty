813c3e964b4114e4aeaff714d5032e053b4625c1
gauravpixxelu <gauravpixxelu@gmail.com>
2023-09-27 15:16:00 +0530

    new\

--- src/Components/FNBMasterUP/FNBMasterUPForm.jsx
+++ src/Components/FNBMasterUP/FNBMasterUPForm.jsx
@@ -9,11 +9,11 @@ const FNBMasterUPForm = () => {
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [business_name, setBusinessName] = useState('');
-    const [business_age, setBusinessAge] = useState('');
-    const [number_of_employee, setEmployes] = useState('');
+    const [business_age, setBusinessAge] = useState('0-5');
+    const [number_of_employee, setEmployes] = useState('0-10');
     const [b_bbee, setBBEE] = useState('');
     const [role, setRole] = useState('');
-    const [annual_turnover, setAnnualTurnover] = useState('');
+    const [annual_turnover, setAnnualTurnover] = useState('0-R10m');
     const [where_did_you, setProgramme] = useState('');
     const [is_accept, setAccept] = useState('');
     const [thankmessage, setThank] = useState('');
@@ -60,11 +60,11 @@ const FNBMasterUPForm = () => {
             setEmail('');
             setPhone('');
             setBusinessName('');
-            setBusinessAge('');
-            setEmployes('');
+            setBusinessAge('0-5');
+            setEmployes('0-10');
             setBBEE('');
             setRole('');
-            setAnnualTurnover('');
+            setAnnualTurnover('0-R10m');
             setProgramme('');
             setAccept('');
         } catch (error) {
@@ -111,10 +111,10 @@ const FNBMasterUPForm = () => {
                     <div className="form-feild">
                         <label className="fnb-label">Business Age (years)</label>
                         <div className="radio-gd">
-                            <div><label><input type="radio" value="0-5" checked={business_age === '0-5'} onChange={(e) => setBusinessAge(e.target.value)} />0-5</label></div>
-                            <div><label><input type="radio" value="5-10" checked={business_age === '5-10'} onChange={(e) => setBusinessAge(e.target.value)} />5-10</label></div>
-                            <div><label><input type="radio" value="10-15" checked={business_age === '10-15'} onChange={(e) => setBusinessAge(e.target.value)} />10-15</label></div>
-                            <div><label><input type="radio" value="15-20" checked={business_age === '15-20'} onChange={(e) => setBusinessAge(e.target.value)} />15-20</label></div>
+                            <div><label><input type="radio" value="0-5" checked={business_age === '0-5'} onChange={(e) => setBusinessAge(e.target.value)} required/>0-5</label></div>
+                            <div><label><input type="radio" value="5-10" checked={business_age === '5-10'} onChange={(e) => setBusinessAge(e.target.value)} required/>5-10</label></div>
+                            <div><label><input type="radio" value="10-15" checked={business_age === '10-15'} onChange={(e) => setBusinessAge(e.target.value)} required/>10-15</label></div>
+                            <div><label><input type="radio" value="15-20" checked={business_age === '15-20'} onChange={(e) => setBusinessAge(e.target.value)} required/>15-20</label></div>
                         </div>
                     </div>
                 </div>
@@ -123,10 +123,10 @@ const FNBMasterUPForm = () => {
                     <div className="form-feild">
                         <label className="fnb-label">Number of Employees</label>
                         <div className="radio-gd">
-                            <div><label><input type="radio" value="0-10" checked={number_of_employee === '0-10'} onChange={(e) => setEmployes(e.target.value)} />0-10</label></div>
-                            <div><label><input type="radio" value="10-20" checked={number_of_employee === '10-20'} onChange={(e) => setEmployes(e.target.value)} />10-20</label></div>
-                            <div><label><input type="radio" value="20-30" checked={number_of_employee === '20-30'} onChange={(e) => setEmployes(e.target.value)} />20-30</label></div>
-                            <div><label><input type="radio" value="30-50" checked={number_of_employee === '30-50'} onChange={(e) => setEmployes(e.target.value)} />30-50</label></div>
+                            <div><label><input type="radio" value="0-10" checked={number_of_employee === '0-10'} onChange={(e) => setEmployes(e.target.value)} required/>0-10</label></div>
+                            <div><label><input type="radio" value="10-20" checked={number_of_employee === '10-20'} onChange={(e) => setEmployes(e.target.value)} required/>10-20</label></div>
+                            <div><label><input type="radio" value="20-30" checked={number_of_employee === '20-30'} onChange={(e) => setEmployes(e.target.value)} required/>20-30</label></div>
+                            <div><label><input type="radio" value="30-50" checked={number_of_employee === '30-50'} onChange={(e) => setEmployes(e.target.value)} required/>30-50</label></div>
                         </div>
                     </div>
                 </div>
@@ -135,7 +135,7 @@ const FNBMasterUPForm = () => {
                     <div className="form-feild">
                         <label className="fnb-label">B-BBEE</label>
                         <select value={b_bbee} onChange={e => setBBEE(e.target.value)} required>
-                            <option value="">B-BBEE</option>
+                            <option value="" disabled>B-BBEE</option>
                             <option value="Level 1">Level 1</option>
                             <option value="Level 2">Level 2</option>
                             <option value="Level 3">Level 3</option>
@@ -152,8 +152,8 @@ const FNBMasterUPForm = () => {
                 <div className="col-md-6">
                     <div className="form-feild">
                         <label className="fnb-label">Role</label>
-                        <select value={role} onChange={e => setRole(e.target.value)}>
-                            <option value="">Role</option>
+                        <select value={role} onChange={e => setRole(e.target.value)} required>
+                            <option value="" disabled>Role</option>
                             <option value="Director">Director</option>
                             <option value="Partner">Partner</option>
                             <option value="Other">Other</option>
@@ -165,10 +165,10 @@ const FNBMasterUPForm = () => {
                     <div className="form-feild">
                         <label className="fnb-label">Annual Turnover</label>
                         <div className="radio-gd">
-                            <div><label><input type="radio" value="0-R10m" checked={annual_turnover === '0-R10m'} onChange={(e) => setAnnualTurnover(e.target.value)} />0-R10m</label></div>
-                            <div><label><input type="radio" value="R10m-R25m" checked={annual_turnover === 'R10m-R25m'} onChange={(e) => setAnnualTurnover(e.target.value)} />R10m-R25m</label></div>
-                            <div><label><input type="radio" value="R25-R35m" checked={annual_turnover === 'R25-R35m'} onChange={(e) => setAnnualTurnover(e.target.value)} />R25-R35m</label></div>
-                            <div><label><input type="radio" value="R35m-R50m" checked={annual_turnover === 'R35m-R50m'} onChange={(e) => setAnnualTurnover(e.target.value)} />R35m-R50m</label></div>
+                            <div><label><input type="radio" value="0-R10m" checked={annual_turnover === '0-R10m'} onChange={(e) => setAnnualTurnover(e.target.value)} required/>0-R10m</label></div>
+                            <div><label><input type="radio" value="R10m-R25m" checked={annual_turnover === 'R10m-R25m'} onChange={(e) => setAnnualTurnover(e.target.value)} required/>R10m-R25m</label></div>
+                            <div><label><input type="radio" value="R25-R35m" checked={annual_turnover === 'R25-R35m'} onChange={(e) => setAnnualTurnover(e.target.value)} required/>R25-R35m</label></div>
+                            <div><label><input type="radio" value="R35m-R50m" checked={annual_turnover === 'R35m-R50m'} onChange={(e) => setAnnualTurnover(e.target.value)} required/>R35m-R50m</label></div>
                         </div>
                     </div>
                 </div>
@@ -177,6 +177,7 @@ const FNBMasterUPForm = () => {
                     <div className="form-feild">
                         <label className="fnb-label">Where did you hear about the programme</label>
                         <select value={where_did_you} onChange={e => setProgramme(e.target.value)} required>
+                        <option value="" disabled>Where did you hear about the programme</option>
                             <option value="FNB Twitter page">FNB Twitter page</option>
                             <option value="FNB Linkedin page">FNB Linkedin page</option>
                             <option value="Linkedin">Linkedin</option>
